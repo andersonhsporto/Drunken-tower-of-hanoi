4e6f3529c94c414b91aeee06d5b877d9184120cd
Anderson Porto <anderson.higo2@gmail.com>
2023-01-21 23:03:08 -0300

    add: solver member function

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include <stack>
 #include <iostream>
+#include <cmath>
 #include "Player.hpp"
 #include "Disk.hpp"
 
@@ -38,11 +39,47 @@ int drunkenTowerOfHanoi(int discs, int tiles, int rodA, int rodB, int rodC, int 
   return sumNumOfTurns / trials;
 }
 
+long simpleSolver(int n, int k, int a, int b, int c) {
+  int g = pow(2, n + 2) - 3 - pow(-1, n);
+
+  g /= 6;
+  return 2 * g * (c - a) * (k - 1) - (2 * k - b - c) * (c - b);
+}
+
+long problemSolver(long k, long a, long b, long c) {
+  long total_awaited_number = 0;
+  long temp = 1;
+  long previous = 0;
+  long current = 1;
+  long mod2 = pow(10, 9);
+
+  while (temp < 10001) {
+    long long awaited_number = ((2 * current * (c - a) * (k - 1)) - ((2 * k - b - c) * (c - b))) % mod2;
+    total_awaited_number = (total_awaited_number + awaited_number) % mod2;
+    long old = (current + (2 * previous) + 1) % mod2;
+    previous = current;
+    current = old;
+    k = (k * 10) % mod2;
+    a = (a * 3) % mod2;
+    b = (b * 6) % mod2;
+    c = (c * 9) % mod2;
+    temp = temp + 1;
+  }
+  return total_awaited_number;
+}
+
 int main() {
   std::cout << "(2,5,1,3,5) has to be 60 and it is: ";
   std::cout << drunkenTowerOfHanoi(2, 5, 1, 3, 5, 10000) << std::endl;
+  std::cout << "(2,5,1,3,5) has to be 60 and it is: ";
+  std::cout << simpleSolver(2, 5, 1, 3, 5) << std::endl;
 
+//  std::cout << "(3,20,4,9,17) has to be 2358 and it is: ";
+//  std::cout << drunkenTowerOfHanoi(3, 20, 4, 9, 17, 100001) << std::endl;
   std::cout << "(3,20,4,9,17) has to be 2358 and it is: ";
-  std::cout << drunkenTowerOfHanoi(3, 20, 4, 9, 17, 100001) << std::endl;
+  std::cout << simpleSolver(3, 20, 4, 9, 17) << std::endl;
+
+  std::cout << "The number is: ";
+  std::cout << problemSolver(10, 3, 6, 9) << std::endl;
   return 0;
 }
